8bea425015f6653d67dd10354bb5dfe41a5ce1f2
keshavraj.patil <keshavraj.patil@NLPULTP2803261.FPS.NIHILENT.com>
2024-06-10 23:11:01 +0530

    First commit....

---
+++ src/GITProject/Keshproject.java
@@ -0,0 +1,21 @@
+package GITProject;
+
+public class Keshproject {
+
+	public static void main(String[] args) {
+		String str="madam";
+		StringBuilder sb=new StringBuilder(str);
+		String abc=sb.reverse().toString();
+		if(str.equals(abc)) {
+			System.out.println("string is palindrome");
+		}else {
+			System.out.println("string is not palindrome");
+		}
+			
+
+	}
+		
+
+	}
+
+
